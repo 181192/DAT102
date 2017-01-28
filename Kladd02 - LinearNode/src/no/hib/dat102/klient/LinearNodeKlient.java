d6d57c718f67cbf0d143f8905384e2c316cb8464
h181192 <h181192@stud.hib.no>
2017-01-28 23:01:24 +0100

    V.0.1.2

--- Kladd02 - LinearNode/src/no/hib/dat102/klient/LinearNodeKlient.java
+++ Kladd02 - LinearNode/src/no/hib/dat102/klient/LinearNodeKlient.java
@@ -11,38 +11,41 @@ import no.hib.dat102.stack.LinkedStack;
  *
  */
 public class LinearNodeKlient {
-	private LinearNode<String> start;
+	
 	public static void main(String[] args) {
 		StackADT<String> sta = new LinkedStack<String>();
-		String[] tab = { "Kari", "Per", "Tine", "Anne", "Person 5", "Person 6", "Person 7", "Person 8", "Person 9" };
-
+		LinearNode<String> start = new LinearNode<String>("Ole");
 		
-		sta.push(tab[0]);
-		sta.push(tab[1]);
-		sta.push(tab[2]);
-		sta.push(tab[3]);
+		sta.push("Kari");
+		sta.push("Per");
+		sta.push("Tine");
+		sta.push("Anne");
 		
 		System.out.println(sta.size());
 
 		// 1. Legg inn ny node som er nr.2 i listen med objekt "Berit"
+		LinearNode<String> nyNode = new LinearNode<String>("Berit");
+		nyNode.setNext(start.getNext());
+		start.setNext(nyNode);
 		
+		System.out.println(sta.size());
 
 		// 2. Anta at listen har fått over 10 noder. Lag en ny node
 		// som er nr.10 (med objekt "Person 10")
-		sta.push(tab[4]);
-		sta.push(tab[5]);
-		sta.push(tab[6]);
-		sta.push(tab[7]);
-		sta.push(tab[8]);
+//		sta.push(tab[4]);
+//		sta.push(tab[5]);
+//		sta.push(tab[6]);
+//		sta.push(tab[7]);
+//		sta.push(tab[8]);
 		
-		String p10 = "Person 10";
+//		String p10 = "Person 10";
 
 		// 3. Legg til ny node helt til slutt i listen med objekt "Sistemann"
-		String sistemann = "Sistemann";
+//		String sistemann = "Sistemann";
 
 		// 4. Fjern node nr.2 i listen
 	
-		
+//		
 		
 		// 5. Fjern siste node i en lang liste
 	} // main
