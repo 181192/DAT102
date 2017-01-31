d86141d610e800dea7a587b94a02aa1942738e89
h181192 <h181192@stud.hib.no>
2017-01-31 12:45:13 +0100

    V.0.2.3

--- Ex01 - CD-Arkiv/src/no/hib/data102/CDArkiv2.java
+++ Ex01 - CD-Arkiv/src/no/hib/data102/CDArkiv2.java
@@ -11,41 +11,42 @@ import no.hib.data102.node.LinearNode;
  * @author Kristoffer-Andre Kalliainen
  *
  */
-public class CDArkiv2 implements CDArkivADT{
-private int antall;
-private LinearNode<CD> start;
+public class CDArkiv2 implements CDArkivADT {
+	private int antall;
+	private LinearNode<CD> start;
+
 	/**
-	 *  Oppretter ett tomt arkiv
+	 * Oppretter ett tomt arkiv
 	 */
 	public CDArkiv2() {
 		antall = 0;
 		start = null;
 	}
-	
+
 	@Override
 	public CD[] hentCDTabell() {
 		LinearNode<CD> node = start;
 		CD[] cdtabell = new CD[antall];
 		int i = 0;
-		
+
 		while (node != null) {
 			cdtabell[i] = node.getElement();
 			node = node.getNeste();
+			i++;
 		}
-		i++;
-		
+
 		return cdtabell;
 	}
 
 	@Override
 	public void leggTilCd(CD nyCd) {
 		LinearNode<CD> temp = new LinearNode<CD>(nyCd);
-		
+
 		temp.setNeste(start);
 		start = temp;
 		antall++;
 	}
-	
+
 	private LinearNode<CD> finnCd(int cdNr) {
 		LinearNode<CD> p = start;
 		while (p != null) {
@@ -57,7 +58,7 @@ private LinearNode<CD> start;
 		}
 		return null;
 	}
-	
+
 	@Override
 	public boolean slettCd(int cdNr) throws EmptyCollectionException {
 		boolean tilstand = false;
@@ -65,8 +66,8 @@ private LinearNode<CD> start;
 			throw new EmptyCollectionException("CDArkiv");
 		}
 		// 1. må søke etter node og sammenlige cdNr
-		LinearNode<CD> resultat= finnCd(cdNr);
-		
+		LinearNode<CD> resultat = finnCd(cdNr);
+
 		// 2. Slette noden/cd'en
 		if (resultat != null) {
 			resultat = resultat.getNeste();
@@ -77,16 +78,42 @@ private LinearNode<CD> start;
 		return tilstand;
 	}
 
+	private CD[] finnFlerCd(String type, String soek) {
+		if (erTom()) {
+			throw new EmptyCollectionException("CDArkiv");
+		}
+		CD[] c = new CD[antall];
+		LinearNode<CD> p = start;
+		int i = 0;
+
+		if (type.equals("Tittel")) {
+			while (p != null) {
+				if (p.getElement().getTittel().contains(soek)) {
+					c[i] = p.getElement();
+					p = p.getNeste();
+					i++;
+				}
+			}
+		} else if (type.equals("Artist")) {
+			while (p != null) {
+				if (p.getElement().getArtist().contains(soek)) {
+					c[i] = p.getElement();
+					p = p.getNeste();
+					i++;
+				}
+			}
+		}
+		return c;
+	}
+
 	@Override
 	public CD[] sokTittel(String delstreng) {
-		// TODO Auto-generated method stub
-		return null;
+		return finnFlerCd("Tittel", delstreng);
 	}
 
 	@Override
 	public CD[] sokArtist(String delstreng) {
-		// TODO Auto-generated method stub
-		return null;
+		return finnFlerCd("Artist", delstreng);
 	}
 
 	@Override
@@ -99,12 +126,13 @@ private LinearNode<CD> start;
 		// TODO Auto-generated method stub
 		return 0;
 	}
-	
+
 	/**
 	 * Skjekker om arkivet er tomt
+	 * 
 	 * @return true visst arkivet er tomt
 	 */
-	private boolean erTom()	{
+	private boolean erTom() {
 		return (start == null);
 	}
 
