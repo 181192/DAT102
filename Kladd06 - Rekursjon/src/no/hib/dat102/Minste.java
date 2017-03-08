71adfe87dcb4c74d21159b2492ba23c525a1b444
h181192 <h181192@stud.hib.no>
2017-03-08 18:36:09 +0100

    V.0.1.1

--- Kladd06 - Rekursjon/src/no/hib/dat102/Minste.java
+++ Kladd06 - Rekursjon/src/no/hib/dat102/Minste.java
@@ -4,7 +4,9 @@ public class Minste {
 	public static void main(String[] args) {
 		int[] tab = { 3242, -222, 332, 11, 43, 54, 4345, 0 };
 		System.out.println("minste rek : " + finneMinste(tab));
-
+		System.out.println("minste rek2: " + finneMinste(tab));
+		
+		// vanlig tabell for å finne minste
 		int minste = tab[0];
 		for (int i = 0; i < tab.length; i++) {
 			if (tab[i] < minste) {
@@ -12,34 +14,49 @@ public class Minste {
 			}
 		}
 		System.out.println("minste tallet : " + minste);
+		
 	}
 
 	public static int finneMinste(int[] tab) {
 		int siste = tab.length - 1;
-		return finneMinsteRek(tab, siste);
+		return finnMinsteRek(tab, siste);
+	}
+	
+	private static int finnMinsteRek(int[] tab, int lengde) {
+	    if (lengde > 0) {
+	        return Math.min(tab[lengde], finnMinsteRek(tab, lengde-1));
+	    } else {
+	        return tab[0];
+	    }
 	}
 
-	private static int finneMinsteRek(int[] tab, int siste) {
-		if (siste == 0) {
-			return tab[siste];
-		}
-		int minste = finneMinsteRek(tab, (siste - 1));
-
-		if (tab[siste] < minste) {
-			return tab[siste];
-		} else {
-			return minste;
-		}
-	}
+//	private static int finneMinsteRek(int[] tab, int siste) {
+//		if (siste == 0) {
+//			return tab[siste];
+//		}
+//		int minste = finneMinsteRek(tab, (siste - 1));
+//
+//		if (tab[siste] < minste) {
+//			return tab[siste];
+//		} else {
+//			return minste;
+//		}
+//	}
 	
 	public static int finneMinste2(int[] tab) {
 		int siste = tab.length - 1;
-		return finnMinste2Rek(tab, siste);
+		int foerste = 0;
+		return finnMinste2Rek(tab, foerste, siste);
 	}
 	
-	private static int finnMinste2Rek(int[] tab, int siste) {
+	private static int finnMinste2Rek(int[] tab, int foerste, int siste) {
+		if (foerste == siste) {
+			return tab[foerste];
+		}
+		int midten = (foerste + siste) / 2;
+		int min1 = finnMinste2Rek(tab, foerste, midten);
+		int min2 = finnMinste2Rek(tab, (midten + 1), siste);
 		
-		int minste = 0;
-		return minste;
+		return Math.min(min1, min2);
 	}
 }
